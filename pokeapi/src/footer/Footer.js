fd3a10cc1dde46307ed0f52d7c80a6c260b9cc02
Jairo Alejandro <jairoalejandro71@gmail.com>
2025-04-19 15:56:53 +0200

    Add files via upload

---
+++ pokeapi/src/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; 
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>© 2025 Pokémon Company. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
+
+export default Footer;
